859f0c698963be6952db80267f129552b118e6b4
DMG <dmwaigithinji@gmail.com>
2019-10-16 12:35:56 +0300

    Toggle-able sidenav on small screen

--- src/app/portfolio/sidenav/sidenav.component.ts
+++ src/app/portfolio/sidenav/sidenav.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
-
+  navActive: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  navOn(){
+    this.navActive = !this.navActive;
+    console.log("Click!");
   }
 
 }
