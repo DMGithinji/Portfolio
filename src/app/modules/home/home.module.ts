285d744c3ba9cc79441b4d07b57fe6454539c686
DMGithinji <dmwaigithinji@gmail.com>
2020-06-05 11:19:38 +0300

    About and hero section added

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -7,11 +7,12 @@ import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
 import { SharedModule } from '@shared/shared.module';
+import { ProjectComponent } from './projects/project/project.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, HeroComponent, AboutComponent, ProjectsComponent, SkillsComponent],
+  declarations: [HomeComponent, HeroComponent, AboutComponent, ProjectsComponent, SkillsComponent, ProjectComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
